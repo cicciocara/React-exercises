b3eb737db7e1d6be781c1b5eccb896e3630a8c00
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-05-11 15:55:50 +0200

    feat/exercise conditional-rendering-05 completed

--- react-exercises/src/Age.js
+++ react-exercises/src/Age.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export class Age extends React.Component {
   render() {
-    return <div>{this.props.children > 18 ? <p>Your age is {this.props.children}</p> : <p>You are very young!</p>}</div>;
+    return <div>{this.props.age > 18 ? <p>Your age is {this.props.age}</p> : <p>You are very young!</p>}</div>;
   }
 }
--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -6,7 +6,7 @@ export class Welcome extends React.Component {
     return (
       <div>
         <p>Welcome {this.props.name}!</p>
-        <Age>{this.props.age}</Age>
+        <Age age={this.props.age}></Age>
       </div>
     );
   }
