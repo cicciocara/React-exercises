c8dcac8cf756c1bdc7716111247fab720bd1ec98
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-05-18 16:53:38 +0200

    feat/exercise styling-components-03 completed

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -18,7 +18,7 @@ export class App extends React.Component {
     return (
       <div>
         <Hello />
-        <UncontrolledLogin actualState={onLogin} />
+        <Login />
       </div>
     );
   }
