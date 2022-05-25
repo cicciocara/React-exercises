6d778fe00ed3baa1dffcb7082fb325da93fdc00f
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-05-25 11:05:02 +0200

    feat/exercise context-01 completed

--- react-exercises/src/DisplayLanguage.js
+++ react-exercises/src/DisplayLanguage.js
@@ -1,26 +1,7 @@
 import React from 'react';
-import { LanguageContext } from './LanguageContext';
-
-const Strings = {
-  en: {
-    CURRENT_LANGUAGE: 'ENGLISH',
-  },
-
-  it: {
-    CURRENT_LANGUAGE: 'ITALIANO',
-  },
-};
 
 export class DisplayLanguage extends React.Component {
   render() {
-    return (
-      <div>
-        <LanguageContext.Consumer>
-          {(language) => {
-            return <h1>{Strings[language].CURRENT_LANGUAGE}</h1>;
-          }}
-        </LanguageContext.Consumer>
-      </div>
-    );
+    return <div></div>;
   }
 }
