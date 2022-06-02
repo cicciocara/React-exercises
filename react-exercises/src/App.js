23ebde2291506ff8f347683afee6c75c66898c3e
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-06-02 10:29:25 +0200

    feat/exercise router-01 completed

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Welcome } from './Welcome';
 
+function onLogin(state) {
+  return console.log(state);
+}
+
+function onChangeCounter(counter) {
+  console.log(`Current counter is now:  ${counter}`);
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+const list = [
+  { name: 'Pisolo', id: 1, age: 21 },
+  { name: 'Brontolo', id: 2, age: 20 },
+  { name: 'Cucciolo', id: 3, age: 19 },
+  { name: 'Eolo', id: 4, age: 12 },
+  { name: 'Mammolo', id: 5, age: 11 },
+  { name: 'Gongolo', id: 6, age: 18 },
+  { name: 'Dotto', id: 7, age: 20 },
+];
+
 export function App() {
+  const [language, setLanguage] = useState('it');
+
+  function handleLanguage(event) {
+    setLanguage(event.target.value);
+  }
+
   return (
     <Routes>
       <Route path='/' element={<Welcome name='cicciocara' />} />
