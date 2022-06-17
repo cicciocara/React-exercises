3acab03f0b52345c68271b5ceb0073700e425fc3
Francesco Caramagno <francesco.caramagno1@gmail.com>
2022-06-17 17:10:36 +0200

    feat/exercise redux-01 completed

--- react-exercises/src/index.js
+++ react-exercises/src/index.js
@@ -3,4 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Root } from './Root';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+import { store } from './state/Store';
+import { incrementCounter, decrementCounter, resetCounter } from './state/CounterState';
+
+//ReactDOM.render(<Root />, document.getElementById('root'));
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(incrementCounter(5));
+store.dispatch(decrementCounter(3));
+store.dispatch(incrementCounter(1));
+store.dispatch(resetCounter());
